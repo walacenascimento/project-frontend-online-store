a1a560776a8b8587fc9a98acecb05cc224280224
Caroline Boaventura <caroline.boaventura08@gmail.com>
2021-08-10 18:56:58 -0300

    Resolve requisito 5

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { getProductsFromCategoryAndQuery } from './services/api';
+import ProductsCard from './ProductsCard';
 
 class MainPage extends React.Component {
   constructor() {
@@ -10,6 +11,7 @@ class MainPage extends React.Component {
     this.state = {
       productInput: '',
       responseAPI: [],
+      loading: true,
     };
   }
 
@@ -24,26 +26,39 @@ class MainPage extends React.Component {
     getProductsFromCategoryAndQuery('', productInput).then((response) => {
       this.setState({
         responseAPI: [...response.results],
+        loading: false,
       });
     });
   }
 
   render() {
-    const { productInput } = this.state;
+    const { productInput, responseAPI, loading } = this.state;
+    const inputEmpty = (
+      <p data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </p>
+    );
+
     return (
       <div>
         <label htmlFor="search-bar-input">
           <input
             type="text"
+            data-testid="query-input"
             name="search-bar-input"
             value={ productInput }
             onChange={ this.handleChange }
           />
         </label>
-        <button type="button" onClick={ this.fetchProduct }>Pesquisar</button>
-        <p data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <button
+          type="button"
+          data-testid="query-button"
+          onClick={ this.fetchProduct }
+        >
+          Pesquisar
+        </button>
+        {loading === true ? inputEmpty : responseAPI
+          .map((product) => <ProductsCard product={ product } key={ product.id } />)}
         <Link to="/shopping-cart">
           <button type="button" data-testid="shopping-cart-button">Cart</button>
         </Link>
